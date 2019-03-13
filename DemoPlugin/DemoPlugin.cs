3e81b87fa7d96cc296c682c005a6fbae2ebec06a
Väinämö Łūmikērø <vainamo@git.festival.ml>
2019-03-13 11:15:35 +0100

    Add demo plugin

---
+++ DemoPlugin/DemoPlugin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Neo.Core.Authentication;
+using Neo.Core.Communication;
+using Neo.Core.Communication.Packages;
+using Neo.Core.Cryptography;
+using Neo.Core.Extensibility;
+using Neo.Core.Extensibility.Events;
+using Neo.Core.Shared;
+
+namespace DemoPlugin
+{
+    public class DemoPlugin : Plugin
+    {
+        public override string Namespace => "ml.festival.demo";
+
+        private Member pluginMember;
+
+        [EventListener(EventType.BeforeInput)]
+        public override async Task OnBeforeInput(Before<InputEventArgs> args) {
+            if (args.Event.Input.StartsWith("/sha ")) {
+                args.Cancel = true;
+
+                args.Event.Sender.ToTarget().SendPackage(new Package(PackageType.Message, MessagePackageContent.GetReceivedMessage(InternalId, pluginMember.Identity, Convert.ToBase64String(NeoCryptoProvider.Instance.Sha512ComputeHash(args.Event.Input.Substring(5))), args.Event.Sender.ActiveChannel.InternalId)));
+            }
+        }
+        
+        public override async Task OnInitialize(string storagePath) {
+            pluginMember = Authenticator.CreateVirtualMember(this);
+            pluginMember.Identity = new Identity {
+                Id = "demo",
+                Name = "Demo Plugin"
+            };
+        }
+    }
+}
